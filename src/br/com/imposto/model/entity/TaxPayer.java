62d8ffe348d9938712fd5baaf39aea5ccfd1c80a
Leandro Vinícius de Azevedo <67933113+leandroviniciusdeazevedo@users.noreply.github.com>
2021-08-19 20:52:34 -0300

    Create TaxPayer.java

---
+++ src/br/com/imposto/model/entity/TaxPayer.java
@@ -0,0 +1,32 @@
+package br.com.imposto.model.entity;
+
+public abstract class TaxPayer {
+    protected String name;
+    protected Double anualIncome;
+
+    public abstract Double tax();
+
+    public TaxPayer() {
+    }
+
+    public TaxPayer(String name, Double anualIncome) {
+        this.name = name;
+        this.anualIncome = anualIncome;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAnualIncome() {
+        return anualIncome;
+    }
+
+    public void setAnualIncome(Double anualIncome) {
+        this.anualIncome = anualIncome;
+    }
+}
